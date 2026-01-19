6c3b994fc0c647f1c42150d591c8804d36b29652
ksell8 <ksell@u.rochester.edu>
2026-01-19 00:48:01 -0500

    add that the hack is a hack and not best practices

--- leccion_3/sample/tests/conftest.py
+++ leccion_3/sample/tests/conftest.py
@@ -2,4 +2,6 @@ import sys
 from pathlib import Path
 
 # Add the sample directory to the path so imports work from any working directory
+# NOTE: This is not how I typically do things.  I typically package my code and install it in editable mode for testing
+# BUT, that's making things more difficult, this is hacky but does the job
 sys.path.insert(0, str(Path(__file__).parent.parent))
